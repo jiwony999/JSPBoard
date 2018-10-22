9466fc507f4509c434ad9324b5ba2d2d50470d33
JiWon Lee <jiwony999@gmail.com>
2018-10-22 10:55:26 +0900

    update 회원가입 check

--- web/include/scripts.js
+++ web/include/scripts.js
@@ -9,53 +9,53 @@ function isCorrectEmail(fname, ename) {
 
     var email = eval("document." + fname + "." + ename); //js 객체로 만듬
 
-    for (i = 0;i<email.value.length;i++){
+    for (i = 0; i < email.value.length; i++) {
 
-        if (email.value.charAt(i) == '@'){
-            position_at +=1;
+        if (email.value.charAt(i) == '@') {
+            position_at += 1;
         }
 
-        else if (position_at == 0){
+        else if (position_at == 0) {
             id += email.value.charAt(i);
         }
 
-        else{
+        else {
             server += email.value.charAt(i);
         }
 
     }
 
 
-    if (position_at>=2 || position_at == 0){
+    if (position_at >= 2 || position_at == 0) {
         alert("postion error");
         return false;
     }
 
-    for (i =  0; i<id.length; i++){
-        if (! ((id.charAt(i) >= 'A' && id.charAt(i) <= 'z') || (id.charAt(i) >='0' && id.charAt(i) <= '9') || (id.charAt(i) =='_') || id.charAt(i) =='-')){
+    for (i = 0; i < id.length; i++) {
+        if (!((id.charAt(i) >= 'A' && id.charAt(i) <= 'z') || (id.charAt(i) >= '0' && id.charAt(i) <= '9') || (id.charAt(i) == '_') || id.charAt(i) == '-')) {
 
             alert("id error");
             return false
         }
     }
 
-    for (i = 0;i<server.length; i++){
-        if (server.charAt(i) == '.'){
+    for (i = 0; i < server.length; i++) {
+        if (server.charAt(i) == '.') {
             dot += 1;
         }
     }
 
-    if (dot<1){
+    if (dot < 1) {
         alert("dot error");
         return false;
     }
 
-    if (server.charAt(server.length-1) == '.'){
+    if (server.charAt(server.length - 1) == '.') {
         alert("server dot error");
         return false;
     }
 
-    for (i = 0;i<server.length; i++) {
+    for (i = 0; i < server.length; i++) {
         if (!((server.charAt(i) >= 'A' && server.charAt(i) <= 'z') || (server.charAt(i) >= '0' && server.charAt(i) <= '9') || (server.charAt(i) == '.') || server.charAt(i) == '-')) {
             alert("server error");
             return false;
@@ -75,7 +75,7 @@ function Korean() {
     }
 
 
-    if (event.keyCode == 8 || event.keyCode == 9||event.keyCode == 35 || event.keyCode == 36 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46) {
+    if (event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 35 || event.keyCode == 36 || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 46) {
         event.returnValue = true;
         return;
     }
@@ -84,4 +84,46 @@ function Korean() {
         event.returnValue = false;
         return false;
     }
+}
+
+// 주민등록번호 형식 조사 함수
+function JuminNoCheck(front, back) {
+    var birthday = front.value;
+    var num = back.value;
+
+    if (birthday != 6) {
+        return false;
+    }
+    if (num.length != 7) {
+        return false;
+    }
+
+    var hap = 0;
+
+    for (var i = 0; i < 6; i++) {
+        var temp = birthday.charAt(i) * (i + 2);
+        hap += temp;
+    }
+
+    var n1 = nun.charAt(0);
+    var n2 = nun.charAt(1);
+    var n3 = nun.charAt(2);
+    var n4 = nun.charAt(3);
+    var n5 = nun.charAt(4);
+    var n6 = nun.charAt(5);
+    var n7 = nun.charAt(6);
+
+    hap += n1 * 8 + n2 * 9 + n3 * 2 + n4 * 3 + n5 * 4 + n6 * 5;
+    hap = hap % 11;
+    hap = 11 - hap;
+
+    if (hap >= 10) {
+        hap = hap % 10;
+    }
+    if (hap != n7) {
+        return false;
+    }
+
+    return true;
+
 }
