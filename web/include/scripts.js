c576c12ba72d467505c34f01c60d6985a290b03d
JiWon Lee <jiwony999@gmail.com>
2018-10-29 13:56:46 +0900

    Update 매개변수 수정

--- web/include/scripts.js
+++ web/include/scripts.js
@@ -171,7 +171,7 @@ function IdCheck(ref, fname, ename, myname, w, h, scroll) {
 
 }
 
-function AddressSearch(ref, fname, ename, myname, w, h, scroll) {
+function AddressSearch(ref, fname, myname, w, h, scroll) {
     var winl = (screen.width - w) / 2;
     var wint = (screen.height - h) / 2;
     winprops = 'height=' + h + ',width=' + w + ',top=' + wint + ',left=' + winl + ',scrollbars=' + scroll;
